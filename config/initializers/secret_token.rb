02fd70439b583bdd645f7d8734e1739036146393
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-02-17 09:16:31 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keks::Application.config.secret_token = '832ea26377aed616bba2697e7e3c2d8891044ab62817b92535c700f2b142381a90fdab50d2823fd76c329d1e7c43fa0a8bfc74c584aca22463544040954f405c'
