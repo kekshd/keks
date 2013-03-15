c9d73d931252384cee765738b1ec53e469b5ccd8
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-03-15 17:57:59 +0100

    asd

--- boot.bash
+++ boot.bash
@@ -9,5 +9,5 @@ export GEM_PATH=/afs/mathi.uni-heidelberg.de/home/keks/gems
 alias ruby="/usr/bin/ruby1.9.1"
 alias gem="/usr/bin/gem1.9.1"
 
-bundle exec rails server -p 10001 -b localhost > /dev/null 2>&1   &
-echo $! > /var/run/keks.pid
+bundle exec rails server -p 10001 -b localhost > /dev/null 2>&1
+#echo $! > /var/run/keks.pid
