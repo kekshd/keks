83287159fd17d3f1661fa38ccdb880dd537b596d
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-03-16 14:13:13 +0100

    fix rails for relative URLs

--- boot.bash
+++ boot.bash
@@ -9,5 +9,11 @@ export GEM_PATH=/afs/mathi.uni-heidelberg.de/home/keks/gems
 alias ruby="/usr/bin/ruby1.9.1"
 alias gem="/usr/bin/gem1.9.1"
 
-bundle exec rails server -p 10001 -b localhost > /dev/null 2>&1
+export RAILS_ENV=development
+#export RAILS_ENV=production
+
+export RAILS_RELATIVE_URL_ROOT=/keks
+
+bundle exec rails server -p 10001 -b localhost >> /srv/keks/log/daemon.log 2>&1
+
 #echo $! > /var/run/keks.pid
