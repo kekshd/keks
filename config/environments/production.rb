63d9f63a2939cc8d0594c14b044a779a5a082de1
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-03-25 12:13:34 +0100

    make production more like dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,13 @@ Keks::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => "www.mathi.uni-heidelberg.de", :protocol => "https:" }
+
+
+
+
+  # make production more like development
+  config.cache_classes = false
+  config.whiny_nils = true
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 end
