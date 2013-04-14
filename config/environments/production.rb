d257e124553756a572e6630c6e27c078d5f6b37c
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-04-14 22:40:32 +0200

    turn on caching in production mode (possible fix for #25, please check)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Keks::Application.configure do
 
 
   # make production more like development
-  config.cache_classes = false
+  #~ config.cache_classes = false
   config.whiny_nils = true
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  #~ config.action_controller.perform_caching = false
 end
