93cbc5a0a1b352022ed49eacd74c0f67ae368c5f
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-08-04 14:29:37 +0200

    fix font size for math jax

--- app/assets/javascripts/mathjax.cfg.js
+++ app/assets/javascripts/mathjax.cfg.js
@@ -7,11 +7,15 @@ $(document).ready(function() {
         emph: '' // no idea how this works, but it makes \emph in math possible
       }
     },
+
     tex2jax: {
       inlineMath: [['$','$'], ['\\(','\\)']],
       skipTags: ["script","noscript","style","textarea","pre","code","td","img"],
       processClass: "tex",
-    }
+    },
+
+    "HTML-CSS": { scale: 80 }
+
   });
 
   MathJax.Hub.Configured();
