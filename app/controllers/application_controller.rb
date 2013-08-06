56d3d5c4ad8dcab014b36f1dd0fbb403e58c9ef2
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-08-06 11:41:11 +0200

    add warning system for page admins for long page loads

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   include LatexHelper
   include DotHelper
   include JsonHelper
+
+ before_filter :init
+  def init
+    @start_time = Time.now.usec
+  end
 end
