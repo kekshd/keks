d7794ec8503db5585333b2163130b7fcdfe3dd84
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-08-06 11:33:14 +0200

    simplify answer_correctness rendering path

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,13 +1,6 @@
 # encoding: utf-8
 
 module AnswersHelper
-  def markup_correct(answer)
-    a = answer || @answer
-    c = a.correct?
-    txt = c ? "✔ richtig" : "✘ falsch"
-    cls = c ? 'success' : 'error'
-    content_tag(:em, txt, class: "alert-#{cls}")
-  end
 
 
 end
