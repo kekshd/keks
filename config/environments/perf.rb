4bf4db487986bf40120574dcb1a4ca9cfaca90a3
Stefan Breunig <stefan-github@yrden.de>
2013-08-30 20:10:21 +0200

    quiet perf profile

--- config/environments/perf.rb
+++ config/environments/perf.rb
@@ -6,11 +6,8 @@ Keks::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
   config.action_mailer.raise_delivery_errors = false
-  config.active_support.deprecation = :log
-  config.action_dispatch.best_standards_support = :builtin
-  config.active_record.mass_assignment_sanitizer = :strict
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_support.deprecation = :notify
+  config.log_level = :warn
   config.assets.compress = false
-  config.assets.debug = true
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
