b5b36e3a3cc6c2be66c15f79836a8b3adfbf8b90
Stefan Breunig <stefan-github@yrden.de>
2013-08-31 14:37:48 +0200

    cleanup

--- db/migrate/20130830203012_more_indexes.rb
+++ db/migrate/20130830203012_more_indexes.rb
@@ -3,9 +3,5 @@ class MoreIndexes < ActiveRecord::Migration
     add_index :stats, :user_id
     add_index :reviews, :question_id
     add_index :starred, [:user_id, :question_id]
-
-
-
-    #~　add_index :questions, :id
   end
 end
