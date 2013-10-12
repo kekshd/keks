af91ed8e608b266098eb23552ce1bf723a210d6d
Stefan Breunig <stefan-github@yrden.de>
2013-10-12 16:55:48 +0200

    handle initial test db setup better

--- test_coverage.sh
+++ test_coverage.sh
@@ -7,8 +7,10 @@ export COVERAGE=true
 USE_THREADS=`/usr/bin/nproc`
 
 echo "Using $USE_THREADS threads"
-echo "Creating test.sqlite3"
-rake "parallel:create[1]" "parallel:prepare[1]" > /dev/null
+if [ ! -s "db/test.sqlite3" ]; then
+  echo "Setting up test DB"
+  zeus rake db:test:prepare
+fi
 
 echo "Copying test.sqlite3 for additional threads"
 for i in `seq 2 $USE_THREADS`; do
--- test_fast.sh
+++ test_fast.sh
@@ -4,10 +4,15 @@ set -e
 
 USE_THREADS=`/usr/bin/nproc`
 
+if [ ! -s "db/test.sqlite3" ]; then
+  echo "Setting up test DB"
+  zeus rake db:test:prepare
+fi
+
 if [ -S ".zeus.sock" ]; then
   echo "Using $USE_THREADS threads"
-  echo "Creating test.sqlite3"
-  zeus rake "parallel:create[1]" "parallel:prepare[1]" > /dev/null
+  #~　echo "Creating test.sqlite3"
+  #~　zeus rake "parallel:create[1]" "parallel:prepare[1]" > /dev/null
 
   echo "Copying test.sqlite3 for additional threads"
   for i in `seq 2 $USE_THREADS`; do
