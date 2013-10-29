8afb6ccf7409679144e22e058c0d9951ac857e70
Stefan Breunig <stefan-github@yrden.de>
2013-10-29 16:43:10 +0100

    more unit tests for xkcd

--- spec/javascripts/xkcd_loader_spec.js
+++ spec/javascripts/xkcd_loader_spec.js
@@ -3,4 +3,34 @@ describe("XkcdLoader", function() {
   it("gives a stub if comic not yet loaded", function() {
     expect(XkcdLoader.retrieve()).toMatch("Laden");
   });
+
+  it("includes class xkcd", function() {
+    expect(XkcdLoader.retrieve()).toMatch('class="xkcd"');
+  });
+
+  it("preloads the comic", function() {
+    runs(function() {
+      mine = XkcdLoader;
+      mine.preload();
+    });
+
+    waits(5000);
+
+    runs(function () {
+      expect(mine.retrieve()).toMatch("xkcd.com");
+    });
+  });
+
+  it("inserts the comic automatically after a while", function() {
+    runs(function() {
+      mine = XkcdLoader;
+      $(mine.retrieve()).appendTo('body');
+    });
+
+    waits(5000);
+
+    runs(function () {
+      expect($('.xkcd').html()).toMatch("xkcd.com");
+    });
+  });
 });
