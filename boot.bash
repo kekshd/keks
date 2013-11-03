489651b9896dc06588d75de5d71f9f71f15e845d
Stefan Breunig <stefan-github@yrden.de>
2013-11-03 15:42:29 +0100

    improve secret token handling (thanks brakeman devs)

--- boot.bash
+++ boot.bash
@@ -13,6 +13,8 @@ alias gem="/usr/bin/gem1.9.1"
 export RAILS_ENV=production
 
 export RAILS_RELATIVE_URL_ROOT=/keks
+[ -e ".SECRET_TOKEN" ] && export SECRET_TOKEN=$(cat .SECRET_TOKEN)
+
 
 bundle exec rails server -p 10001 -b localhost --daemon >> /srv/keks/log/daemon.log 2>&1
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,8 @@
-# Be sure to restart your server when you modify this file.
+# the secret token should not be check into the repository. For dev and
+# test mode a dummy token suffices. In production either
+#      export SECRET_TOKEN="random chars here"
+# or put it into
+#      apg -a 1 -n 1 -m 60 -x 60 -M cLn > .SECRET_TOKEN
+# the .SECRET_TOKEN file. The token should not be changed regularly.
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Keks::Application.config.secret_token = '832ea26377aed616bba2697e7e3c2d8891044ab62817b92535c700f2b142381a90fdab50d2823fd76c329d1e7c43fa0a8bfc74c584aca22463544040954f405c'
+Keks::Application.config.secret_token = Rails.env.production? ? ENV['SECRET_TOKEN'] : "x"*30
