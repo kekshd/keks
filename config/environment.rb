051365569baef35b3d189662559f151dccaf1b21
Stefan Breunig <stefan-github@yrden.de>
2013-11-12 13:00:29 +0100

    also include hints in last admin/reviewer change status

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 def last_admin_or_reviewer_change
-  t = [:categories_last_update, :questions_last_update, :answers_last_update,:reviews_last_update]
+  t = [:categories_last_update, :questions_last_update, :answers_last_update, :reviews_last_update, :hints_last_update]
   t = t.map { |x| Rails.cache.fetch(x) { Time.now } }
   t.max
 end
