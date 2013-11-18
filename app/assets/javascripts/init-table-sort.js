ff657f630bcb9b95220b7857597d16bf9d6cd8c2
Stefan Breunig <stefan-github@yrden.de>
2013-11-18 11:49:05 +0100

    allow tablesorter to run on ajax load

--- app/assets/javascripts/init-table-sort.js
+++ app/assets/javascripts/init-table-sort.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
   $(".tablesorter").addClass('tablesorter-dropbox').tablesorter();
 });
+
+$(document).ajaxComplete(function() {
+  $(".tablesorter").addClass('tablesorter-dropbox').tablesorter();
+});
