fe294935fb7d3e8175d690edde4cd3a4686e95ce
Stefan Breunig <stefan-github@yrden.de>
2013-11-24 14:17:26 +0100

    read all last update values at once, which may be benificial if we switch the cache backend

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,14 @@
 require File.expand_path('../application', __FILE__)
 
 def last_admin_or_reviewer_change
-  t = [:categories_last_update, :questions_last_update, :answers_last_update, :reviews_last_update, :hints_last_update]
-  t = t.map { |x| Rails.cache.fetch(x) { Time.now } }
-  t.max
+  t = Rails.cache.read_multi(
+    :categories_last_update,
+    :questions_last_update,
+    :answers_last_update,
+    :reviews_last_update,
+    :hints_last_update
+  )
+  t.values.max
 end
 
 
