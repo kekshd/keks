40a545e45a37f565601a4ea94e4de20ead1a16aa
Stefan Breunig <stefan-github@yrden.de>
2014-05-10 09:02:10 +0200

    clear out more uninteresting files

--- script/push_bundler.zsh
+++ script/push_bundler.zsh
@@ -12,7 +12,10 @@ rvm use 1.9.3
 bundle install --without development --deployment --jobs 4
 
 rm -rf vendor/bundle/**/gems/nokogiri-*/ext/nokogiri/tmp
+rm -rf vendor/bundle/**/gems/nokogiri-*/ports/*/libxml2/*/share/*doc
 rm -rf vendor/bundle/**/gems/capybara-webkit-*/src/webkit_server.gch/c++
+rm -rf vendor/bundle/**/gems/teaspoon-*/spec/dummy
+rm -rf vendor/bundle/ruby/*/cache
 tar cf pack_bundler.tar vendor/bundle
 xz --verbose -9e pack_bundler.tar
 rsync --partial --progress "./pack_bundler.tar.xz" vollmar-stefan:/srv/trap/share/bundler/keks.tar.xz
